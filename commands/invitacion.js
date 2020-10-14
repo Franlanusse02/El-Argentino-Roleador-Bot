c465ed37fbe8e07e2bb67e72c08920c77e16cb70
Fran Lanusse <52051576+Franlanusse02@users.noreply.github.com>
2020-10-14 15:21:48 -0300

    Update invitacion.js

--- commands/invitacion.js
+++ commands/invitacion.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'invitacion',
     description: 'Manda invitacion al server',
     execute(message, args){
-        message.channel.send('https://discord.gg/cnd47p');
+        message.channel.send('https://discord.gg/nFrS2x6');
     }
 }
