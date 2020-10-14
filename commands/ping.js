136d8780158a0dc86f0581b237e8fc4229311d3e
Fran Lanusse <52051576+Franlanusse02@users.noreply.github.com>
2020-10-14 14:59:38 -0300

    jaj

--- commands/ping.js
+++ commands/ping.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'ping',
     description: 'Ping command',
     execute(message, args){
-        message.channel.send('Uuu loco no te cansas de romper los huevos?!');
+        message.channel.send('Pong!');
     }
 }
