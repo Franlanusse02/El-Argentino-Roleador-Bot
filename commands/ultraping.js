a471ec0068fb16f2db1660392008ad1995922329
Fran Lanusse <52051576+Franlanusse02@users.noreply.github.com>
2020-10-14 14:54:15 -0300

    update ultraping

--- commands/ultraping.js
+++ commands/ultraping.js
@@ -2,7 +2,6 @@ module.exports = {
     name: 'ultraping',
     description: 'Remate mano de dios.',
     execute(message, args){
-        message.channel.send('Fuaa loco, que fuerza tenes! Asi no puedo devolverte!',
-         'Toma, aca tenes 5 p pa tomar ese vi... eee digo por ganarme jaja.');
+        message.channel.send('Fuaa loco, que fuerza tenes! Asi no puedo devolverte! Toma, aca tenes 5 p pa tomar ese vi... eee digo por ganarme jaja.');
     }
 }
