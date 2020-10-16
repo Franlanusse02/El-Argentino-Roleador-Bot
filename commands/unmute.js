fbca0f46553bcb002e6cca879db1e2455e89a761
Fran Lanusse <52051576+Franlanusse02@users.noreply.github.com>
2020-10-16 11:23:59 -0300

    Update unmute.js

--- commands/unmute.js
+++ commands/unmute.js
@@ -8,18 +8,19 @@ module.exports = {
         let mainRole = '685937885228040228';
         let muteRole = '717544216380833894';
         let modRole = '685934052943200385';
+        let helperRole = '718632822532735000'
 
         if (!args[0]){
             message.channel.send('Nadie fue seleccionado para desmutear.');
-        } else if(!message.member.roles.cache.has(modRole)){
+        } else if(!message.member.roles.cache.has(modRole) or !message.member.roles.cache.has(helperRole)){
             message.channel.send('No tenes permiso para usar este comando.');
         } else if(!muteMember) {
             message.channel.send('Esa persona no esta en el server.');
-        } else if(message.member.roles.cache.has(modRole) || muteMember.roles.cache.has(muteRole)){
+        } else if(message.member.roles.cache.has(modRole) or message.member.roles.cache.has(helperRole) || muteMember.roles.cache.has(muteRole)){
             muteMember.roles.add(mainRole);
             muteMember.roles.remove(muteRole);  
             message.channel.send(`${muteMember.user} fue desmuteado.`);
-        } else if(message.member.roles.cache.has(modRole) || !muteMember.roles.cache.has(muteRole)){
+        } else if(message.member.roles.cache.has(modRole) or message.member.roles.cache.has(helperRole) || !muteMember.roles.cache.has(muteRole)){
             message.channel.send('La persona no esta muteada');
          
         } else {
