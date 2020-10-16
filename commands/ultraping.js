0c43bad1ada6665fdeccf241b348696ab9301dab
Fran Lanusse <52051576+Franlanusse02@users.noreply.github.com>
2020-10-16 11:05:41 -0300

    Update ultraping.js

--- commands/ultraping.js
+++ commands/ultraping.js
@@ -3,7 +3,7 @@ module.exports = {
     description: 'Remate mano de dios.',
     execute(message, args){
         var respuestas = ['Fuaa loco, que fuerza tenes! Asi no puedo devolverte! Toma, aca tenes 5 p pa tomar ese vi... eee digo por ganarme jaja.', 'Tomaaaaa! Te ganeeee! Sos malisimo pibe.'];
-        var aleatorio = Math.floor(Math.random()*(Mensajes.length));
+        var aleatorio = Math.floor(Math.random()*(respuestas.length));
         message.channel.send(respuestas[aleatorio]);
     }
 }
