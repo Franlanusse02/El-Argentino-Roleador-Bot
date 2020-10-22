66bbed723068c5782cc9f28121278cafcb844380
HMoon1894 <73201558+HMoon1894@users.noreply.github.com>
2020-10-22 20:50:58 -0300

    Update miembros.js

--- commands/miembros.js
+++ commands/miembros.js
@@ -3,7 +3,18 @@ module.exports = {
     description: 'Contador de gatos locos',
     execute(message, args){
         const Discord = require('discord.js');
-		        message.channel.send(`Usted se encuentra en: ${message.guild.name}\nActualmente somos: ${message.guild.memberCount} gatos locos \nDisfruta de tu estancia! (mientras puedas)`);
+	   const miembros = new Discord.MessageEmbed()
+    .setColor('#70cbff')
+   .setTitle("Hola! (re cortés el bot, nada que ver)")
+   .setThumbnail('https://i.ibb.co/PNszQD2/494b01e4ab2f472ec856787860a0f989.png')
+   .setAuthor('El argentino roleador', 'https://i.ibb.co/PNszQD2/494b01e4ab2f472ec856787860a0f989.png')
+   	.addFields(
+		{ name: 'Usted se encuentra en', value:`${message.guild.name}` },
+		{ name: "Actualmente somos", value:`${message.guild.memberCount} gatos locos`},
+		{ name: "Disfruta de tu estancia (mientras puedas)", value: ":maradonado:"},
+
+	)
+     message.channel.send(miembros);
 
     }
 }
