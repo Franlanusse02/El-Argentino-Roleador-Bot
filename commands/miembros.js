c0784a9c760c7c7089662d6ba6afeb06918a5846
HMoon1894 <73201558+HMoon1894@users.noreply.github.com>
2020-10-23 12:02:51 -0300

    Update miembros.js

--- commands/miembros.js
+++ commands/miembros.js
@@ -3,6 +3,7 @@ module.exports = {
     description: 'Contador de gatos locos',
     execute(message, args){
         const Discord = require('discord.js');
+		 const maradonado = client.emojis.cache.find(emoji => emoji.name === `maradonado`);
 	   const miembros = new Discord.MessageEmbed()
     .setColor('#70cbff')
    .setTitle("Hola! (re cortés el bot, nada que ver)")
@@ -11,7 +12,7 @@ module.exports = {
    	.addFields(
 		{ name: 'Usted se encuentra en', value:`${message.guild.name}` },
 		{ name: "Actualmente somos", value:`${message.guild.memberCount} gatos locos`},
-		{ name: "Disfruta de tu estancia (mientras puedas)", value: `:maradonado:`
+		{ name: "Disfruta de tu estancia (mientras puedas)", value: `${maradonado}`
 
 	)
      message.channel.send(miembros);
